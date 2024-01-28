cc1a0f5984cdd2f2912f9fe000b008650d484b37
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-28 15:33:01 +0800

    homework

--- src/main/java/ru/otus/java/homework/javaHomework.java
+++ src/main/java/ru/otus/java/homework/javaHomework.java
@@ -11,6 +11,45 @@ import java.util.stream.Collectors;
 public class javaHomework {
     public static void main(String[] args) {
 
+        var file = new File(System.getProperty("user.dir"));
+
+        FilenameFilter filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File f, String name) {
+                return name.endsWith(".txt");
+            }
+        };
+
+        System.out.println("Список файлов: " + Arrays.stream(file.listFiles(filter)).map(it -> it.getName()).collect(Collectors.<String>toList()));
+
+
+        System.out.println("Введите имя файла:");
+        Scanner txt = new Scanner(System.in);
+        String name = txt.nextLine();
+
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(name))) {
+            int n = in.read();
+            while (n != -1) {
+                System.out.print((char)n);
+                n = in.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+        System.out.println("Введите текст для записи:");
+        String data = txt.nextLine();
+
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(name))) {
+            byte[] buffer = data.getBytes(StandardCharsets.UTF_8);
+            for (int i = 0; i < buffer.length; i++) {
+                out.write(buffer[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
