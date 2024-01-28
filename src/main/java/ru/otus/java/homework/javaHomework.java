d5d0e104c74f4cbbc432f5bcd77952a049866537
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-28 15:29:58 +0800

    Initial commit

---
+++ src/main/java/ru/otus/java/homework/javaHomework.java
@@ -0,0 +1,16 @@
+package ru.otus.java.homework;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class javaHomework {
+    public static void main(String[] args) {
+
+
+    }
+}
